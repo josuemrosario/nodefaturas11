24ebc1984688f6012c7ae3a347c658132760a962
Josue Micaroni do Rosario <josue.rosario@docente.senai.br>
2025-09-26 08:15:52 -0300

    tutorial 7 implementado

---
+++ models/UsuarioModel.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const UsuarioSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true },
+    password: {
+        type: String,
+        required: true },
+})
+
+const Usuario = model('Usuario', UsuarioSchema);
+
+module.exports = Usuario;
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,17 +1,12 @@
 const express = require('express');
+const usuarioController = require('../controllers/UsuarioController.js');
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-    res.render('index',{mensagem:'get /usuario'}) 
-});
-
-router.get('/criar', (req, res) => {
-    res.render('index',{mensagem:'get /usuario/criar'}) 
-});
-router.get('/deletar', (req, res) => {
-    res.render('index',{mensagem:'get /usuario/deletar'}) 
-});
+router.get('/', usuarioController.obterUsuario);
+router.get('/criar', usuarioController.criarUsuario);
+router.get('/alterar', usuarioController.alterarUsuario);
+router.get('/deletar', usuarioController.deletarUsuario);
 
 module.exports = router;
 
